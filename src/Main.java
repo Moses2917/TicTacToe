0a77663413391820f7879292c19b1eb41f783632
Movses Movsesyan <65843759+Moses2917@users.noreply.github.com>
2024-08-27 15:16:01 -0700

    Starting tictactoe project

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+//package csc133;
+import java.io.*;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("Hello and welcome!");
+        String str = new Scanner(System.in).nextLine();
+        System.out.println("One sec..." + str + "\nInput:");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Welcome " + scanner.next());
+    }
+}
