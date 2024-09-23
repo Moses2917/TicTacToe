66b16e6361a7b1f4b8abec5d0b43400599554c07
MOSO <moses2917@gmail.com>
2024-09-23 16:03:05 -0700

    Added frontend messages
    Signed-off-by: MOSO <moses2917@gmail.com>

--- src/csc133/gameFE.java
+++ src/csc133/gameFE.java
@@ -2,15 +2,9 @@ package csc133;
 import csc133.spot.*;
 import java.util.Scanner;
 public class gameFE {
-    private final int[][] game_board;
-    Scanner sc = new Scanner(System.in);
-
-    public gameFE() {
-        game_board = new int[3][3];
-
-    }
 
     public boolean promptToStart() {
+        Scanner sc = new Scanner(System.in);
         System.out.println("Hello and welcome to my Tic Tac Toe game!");
         System.out.println("Rules:\n----------------------\nWe here at MovsesyanGamesStudio© Inc pride ourselves by being a 0 index game studio.");
         System.out.println("That means when entering where you wish to mark on the board start counting from 0 instead of 1.");
@@ -20,6 +14,27 @@ public class gameFE {
         if (user_input.equalsIgnoreCase("N")) {
             return true;
         }
-        return true;
+        else {
+            return false;
+        }
+    }
+
+    public void print_exit_message(int gameStatus) {
+        switch (gameStatus) {
+            case 1:
+                System.out.println("Sorry to see you go; come again!");
+                break;
+            case 2:
+                System.out.println("Congratulations! you have won!");
+                break;
+            case 3:
+                System.out.println("Sorry, you did not win; try again!");
+                break;
+            case 4:
+                System.out.println("Hey, you almost beat me, let's try again!");
+                break;
+            default: // case: game incomplete
+                break;
+        }
     }
 }
